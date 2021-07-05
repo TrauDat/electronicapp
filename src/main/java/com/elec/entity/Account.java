e22191ad8780d002eb10f6d1ec93c58a969ff0f0
TrauDat <traudat2212@gmail.com>
2021-07-05 11:18:55 +0700

    add entity account

---
+++ src/main/java/com/elec/entity/Account.java
@@ -0,0 +1,43 @@
+package com.elec.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="Account")
+@Data
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id", updatable = false, nullable = false)
+	private long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private LocalDate dob;
+	
+	private String gender;
+	
+	private String role;
+	
+	private String email;
+	
+	private String password;
+	
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", dob=" + dob + ", email="
+				+ email + "]";
+	}
+}
